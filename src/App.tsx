c49e4a4898e28fe5d3386e70c41cde940062a9cd
Luke <thth004d@spgroup.com.sg>
2023-12-29 13:46:43 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Slider from "@mui/material/Slider";
+import PopoverMenu from "./PopoverMenu";
+import ProTip from "./ProTip";
+import { KanbanBoard } from "./components/composites";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <KanbanBoard />
+    </Container>
+  );
+}
