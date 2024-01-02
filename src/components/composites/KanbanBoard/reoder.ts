07f5f3a83e964dd5c242e06543f4409a49e68fa3
Luke <thth004d@spgroup.com.sg>
2024-01-02 10:05:16 +0700

    update attr

--- src/components/composites/KanbanBoard/reoder.ts
+++ src/components/composites/KanbanBoard/reoder.ts
@@ -23,9 +23,12 @@ export const reorderTasks = ({
 }) => {
   const current = [...tasks[source.droppableId]];
   const next = [...(tasks[destination.droppableId] || [])];
-  const target = current[source.index];
+  const target: Task = {
+    ...current[source.index],
+  };
   // moving to same list
   if (source.droppableId === destination.droppableId) {
+    swapAttr(current, source.index, destination.index, "sort");
     const reordered = reorder(current, source.index, destination.index);
     const result = {
       ...tasks,
@@ -37,7 +40,22 @@ export const reorderTasks = ({
   }
 
   current.splice(source.index, 1);
+  current.forEach((item, index) => {
+    if (index < source.index) {
+      return;
+    }
+    item.sort--;
+  });
+  target.column_id = destination.droppableId;
   next.splice(destination.index, 0, target);
+  let currSort = next[destination.index - 1]?.sort || 0;
+  next.forEach((item, index) => {
+    if (index < source.index) {
+      return;
+    }
+    currSort++;
+    item.sort = currSort;
+  });
   const result = {
     ...tasks,
     [source.droppableId]: current,
@@ -47,3 +65,18 @@ export const reorderTasks = ({
     tasks: result,
   };
 };
+
+function swapAttr<D>(
+  data: D[],
+  sourceIndex: number,
+  destIndex: number,
+  attr: keyof D
+): D[] {
+  if (!data || !data[sourceIndex] || !data[destIndex]) {
+    return data;
+  }
+  const temp = data[sourceIndex][attr];
+  data[sourceIndex][attr] = data[destIndex][attr];
+  data[destIndex][attr] = temp;
+  return data;
+}
